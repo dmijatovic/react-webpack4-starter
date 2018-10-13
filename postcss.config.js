57c49a6d1c415fa3378261742ac28cdcd1f97d36
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2018-10-13 10:58:42 +0200

    upgrade postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  plugins: [
-    require('autoprefixer')
+module.exports={
+  plugins:[
+    require('postcss-preset-env')({
+      autoprefixer:{
+        grid: true,
+        flexbox: true
+      },
+      stage:3
+    })
   ]
 }
